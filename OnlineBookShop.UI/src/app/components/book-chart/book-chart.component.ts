3883f52d049b3cf33480f57bb4c8a1c5d100e8df
juniorC0 <gavrilyukmaks0@gmail.com>
2023-03-24 14:29:58 +0200

    Added book-chart

--- OnlineBookShop.UI/src/app/components/book-chart/book-chart.component.ts
+++ OnlineBookShop.UI/src/app/components/book-chart/book-chart.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { ChartDataset } from 'chart.js/auto';
-import { ChartOptions, ChartType } from 'chart.js/auto';
+import { Component } from '@angular/core';
+import { ChartConfiguration } from 'chart.js/auto';
 import { BookService } from 'src/app/services/book.service';
 
 @Component({
@@ -8,25 +7,25 @@ import { BookService } from 'src/app/services/book.service';
   templateUrl: './book-chart.component.html',
   styleUrls: ['./book-chart.component.css'],
 })
-export class BookChartComponent implements OnInit {
-  public barChartOptions: ChartOptions = {
-    responsive: true,
-  };
-
-  public barChartLabels: string[] = [];
-  public barChartType: ChartType = 'bar';
+export class BookChartComponent {
   public barChartLegend = true;
   public barChartPlugins = [];
-  public barChartData: ChartDataset[] = [
-    { data: [], label: 'Number of books' },
-  ];
-
+  public barChartData: ChartConfiguration<'bar'>['data'] = {
+    labels: [],
+    datasets: [],
+  };
+  public barChartOptions: ChartConfiguration<'bar'>['options'] = {
+    responsive: false,
+  };
   constructor(private bookService: BookService) {}
-
   ngOnInit(): void {
     this.bookService.getBooksByYear().subscribe((booksByYear) => {
-      this.barChartLabels = booksByYear.map((data) => data.year.toString());
-      this.barChartData[0].data = booksByYear.map((data) => data.count);
+      this.barChartData.labels = booksByYear.map((data) =>
+        data.year.toString()
+      );
+      this.barChartData.datasets[0].data = booksByYear.map(
+        (data) => data.count
+      );
     });
   }
 }
